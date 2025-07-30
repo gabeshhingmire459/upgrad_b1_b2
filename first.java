e70cf787afe0785f3607ab832df89a867a11b48e
Ganesh Hingmire <Ganesh>
2025-07-30 17:52:19 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first 
+public staticc void main ( string args []) args {
+    system.out.println (x:"Hello from the first class");
+    system.out.println (x: "seccond claass")
+}
+
+}
