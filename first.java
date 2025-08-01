0e37da8c3951f4a8c189058f41abf74ad36a9b3b
Ganesh Hingmire <Ganesh>
2025-08-01 17:49:46 +0530

    new Branch added

--- first.java
+++ first.java
@@ -1,7 +1,9 @@
 public class first 
-public staticc void main ( string args []) args {
+public staticc void main (string args[]) args {
     system.out.println (x:"Hello from the first class");
-    system.out.println (x: "seccond claass")
+    system.out.println (x: "seccond claass");
+    system.out.println (x:"output of the second file");
+    system.out.println (x:"output of first and second cclasss");
 }
 
 }
