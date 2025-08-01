0367464b09f4aec51a91513cb0d59826506f5b46
Ganesh Tukaram Hingamire <149803825+gabeshhingmire459@users.noreply.github.com>
2025-08-01 18:18:46 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,6 +2,7 @@ public class first
 public staticc void main ( string args []) args {
     system.out.println (x:"Hello from the first class");
     system.out.println (x: "seccond claass")
+        int a =8;
 }
 
 }
